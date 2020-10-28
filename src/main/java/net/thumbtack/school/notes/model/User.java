2173063d96bcb8ce9bc468c5b36f016988437631
Дмитрий Патока <dmitrypatoka@ya.ru>
2020-10-29 01:24:16 +0600

    User: more fields

--- src/main/java/net/thumbtack/school/notes/model/User.java
+++ src/main/java/net/thumbtack/school/notes/model/User.java
@@ -1,6 +1,7 @@
 package net.thumbtack.school.notes.model;
 
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -12,13 +13,18 @@ public class User {
     private String patronymic;
     private String lastName;
     private Role role;
+    private List<Note> notes;
+    private List<Comment> comments;
+    private List<Rating> ratings;
+    private List<Section> sections;
     
     
     public User() {
     }
     
     
-    public User(Integer id, String login, String password, String firstName, String patronymic, String lastName, Role role) {
+    public User(Integer id, String login, String password, String firstName, String patronymic, String lastName, Role role,
+                List<Note> notes, List<Comment> comments, List<Rating> ratings, List<Section> sections) {
         setId(id);
         setLogin(login);
         setPassword(password);
@@ -26,11 +32,16 @@ public class User {
         setPatronymic(patronymic);
         setLastName(lastName);
         setRole(role);
+        setNotes(notes);
+        setComments(comments);
+        setRatings(ratings);
+        setSections(sections);
     }
     
     
-    public User(String login, String password, String firstName, String patronymic, String lastName, Role role) {
-        this(null, login, password, firstName, patronymic, lastName, role);
+    public User(String login, String password, String firstName, String patronymic, String lastName, Role role,
+                List<Note> notes, List<Comment> comments, List<Rating> ratings, List<Section> sections) {
+        this(null, login, password, firstName, patronymic, lastName, role, notes, comments, ratings, sections);
     }
     
     
@@ -69,6 +80,26 @@ public class User {
     }
     
     
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+    
+    
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    
+    
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+    
+    
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
+    }
+    
+    
     public Integer getId() {
         return id;
     }
@@ -104,6 +135,26 @@ public class User {
     }
     
     
+    public List<Note> getNotes() {
+        return notes;
+    }
+    
+    
+    public List<Comment> getComments() {
+        return comments;
+    }
+    
+    
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+    
+    
+    public List<Section> getSections() {
+        return sections;
+    }
+    
+    
     @Override
     public String toString() {
         return "User{" +
@@ -125,12 +176,16 @@ public class User {
                 Objects.equals(getFirstName(), user.getFirstName()) &&
                 Objects.equals(getPatronymic(), user.getPatronymic()) &&
                 Objects.equals(getLastName(), user.getLastName()) &&
-                getRole() == user.getRole();
+                getRole() == user.getRole() &&
+                Objects.equals(getNotes(), user.getNotes()) &&
+                Objects.equals(getComments(), user.getComments()) &&
+                Objects.equals(getRatings(), user.getRatings()) &&
+                Objects.equals(getSections(), user.getSections());
     }
     
     
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getLogin(), getPassword(), getFirstName(), getPatronymic(), getLastName(), getRole());
+        return Objects.hash(getId(), getLogin(), getPassword(), getFirstName(), getPatronymic(), getLastName(), getRole(), getNotes(), getComments(), getRatings(), getSections());
     }
 }
