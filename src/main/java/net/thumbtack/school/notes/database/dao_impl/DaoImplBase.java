69dc1a7e7d0be96d349392620ec1121606af394f
Дмитрий Патока <dmitrypatoka@ya.ru>
2020-11-08 03:08:25 +0600

    DaoImplBase

---
+++ src/main/java/net/thumbtack/school/notes/database/dao_impl/DaoImplBase.java
@@ -0,0 +1,48 @@
+package net.thumbtack.school.notes.database.dao_impl;
+
+
+import net.thumbtack.school.notes.database.mapper.*;
+import net.thumbtack.school.notes.database.util.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+
+
+public class DaoImplBase {
+    protected SqlSession getSession() {
+        return MyBatisUtil.getSqlSessionFactory().openSession();
+    }
+    
+    
+    protected CommentMapper getCommentMapper(SqlSession session) {
+        return session.getMapper(CommentMapper.class);
+    }
+    
+    
+    protected NoteMapper getNoteMapper(SqlSession session) {
+        return session.getMapper(NoteMapper.class);
+    }
+    
+    
+    protected NoteRevisionMapper getNoteRevisionMapper(SqlSession session) {
+        return session.getMapper(NoteRevisionMapper.class);
+    }
+    
+    
+    protected RatingMapper getRatingMapper(SqlSession session) {
+        return session.getMapper(RatingMapper.class);
+    }
+    
+    
+    protected SectionMapper getSectionMapper(SqlSession session) {
+        return session.getMapper(SectionMapper.class);
+    }
+    
+    
+    protected SessionMapper getSessionMapper(SqlSession session) {
+        return session.getMapper(SessionMapper.class);
+    }
+    
+    
+    protected UserMapper getUserMapper(SqlSession session) {
+        return session.getMapper(UserMapper.class);
+    }
+}
