6f8ea0827e58cd17f252725efb701c46c930667e
Дмитрий Патока <dmitrypatoka@ya.ru>
2020-11-12 03:05:51 +0600

    UserDao w/o tests

--- src/main/java/net/thumbtack/school/notes/database/dao/UserDao.java
+++ src/main/java/net/thumbtack/school/notes/database/dao/UserDao.java
@@ -2,6 +2,8 @@ package net.thumbtack.school.notes.database.dao;
 
 
 import net.thumbtack.school.notes.model.User;
+import net.thumbtack.school.notes.model.UserType;
+import net.thumbtack.school.notes.view.UserView;
 
 import java.util.List;
 
@@ -9,6 +11,14 @@ import java.util.List;
 public interface UserDao {
     void insert(User user);
     void update(User user);
+    
     User get(int id);
     List<User> getAll();
+    List<UserView> getAllWithRating(String sortByRating, boolean selectSuper, Integer from, Integer count);
+    List<UserView> getAllByType(UserType userType, String sortByRating, boolean selectSuper, Integer from, Integer count);
+    List<UserView> getAllByRelationToUser(User user, String relation,
+                                          String sortByRating, boolean selectSuper, Integer from, Integer count);
+    
+    void delete(User user);
+    void deleteAll();
 }
--- src/main/java/net/thumbtack/school/notes/database/dao_impl/UserDaoImpl.java
+++ src/main/java/net/thumbtack/school/notes/database/dao_impl/UserDaoImpl.java
@@ -3,6 +3,8 @@ package net.thumbtack.school.notes.database.dao_impl;
 
 import net.thumbtack.school.notes.database.dao.UserDao;
 import net.thumbtack.school.notes.model.User;
+import net.thumbtack.school.notes.model.UserType;
+import net.thumbtack.school.notes.view.UserView;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -72,6 +74,87 @@ public class UserDaoImpl extends DaoImplBase implements UserDao {
         } catch (RuntimeException e) {
             LOGGER.info("Cannot get all users", e);
             throw e;
+        }
+    }
+    
+    
+    @Override
+    public List<UserView> getAllWithRating(String sortByRating, boolean selectSuper, Integer from, Integer count) {
+        LOGGER.debug("Getting all users with rating (sortByRating={}, selectSuper={}, from={}, count={})",
+                sortByRating, selectSuper, from, count);
+        
+        try (SqlSession session = getSession()) {
+            return getUserMapper(session).getAllWithRating(sortByRating, selectSuper, from, count);
+        } catch (RuntimeException e) {
+            LOGGER.info("Cannot get all users with rating (sortByRating={}, selectSuper={}, from={}, count={})",
+                    sortByRating, selectSuper, from, count, e);
+            throw e;
+        }
+    }
+    
+    
+    @Override
+    public List<UserView> getAllByType(UserType userType, String sortByRating, boolean selectSuper, Integer from, Integer count) {
+        LOGGER.debug("Getting all users by type {} (sortByRating={}, selectSuper={}, from={}, count={})",
+                userType, sortByRating, selectSuper, from, count);
+        
+        try (SqlSession session = getSession()) {
+            return getUserMapper(session).getAllByType(userType, sortByRating, selectSuper, from, count);
+        } catch (RuntimeException e) {
+            LOGGER.info("Cannot get all users by type {} (sortByRating={}, selectSuper={}, from={}, count={})",
+                    userType, sortByRating, selectSuper, from, count, e);
+            throw e;
+        }
+    }
+    
+    
+    @Override
+    public List<UserView> getAllByRelationToUser(User user, String relation, String sortByRating, boolean selectSuper, Integer from, Integer count) {
+        LOGGER.debug("Getting all users by relation {} to {} (sortByRating={}, selectSuper={}, from={}, count={})",
+                relation, user, sortByRating, selectSuper, from, count);
+    
+        try (SqlSession session = getSession()) {
+            return getUserMapper(session).getAllByRelationToUser(user, relation, sortByRating, selectSuper, from, count);
+        } catch (RuntimeException e) {
+            LOGGER.info("Cannot get all users by relation {} to {} (sortByRating={}, selectSuper={}, from={}, count={}",
+                    relation, user, sortByRating, selectSuper, from, count, e);
+            throw e;
+        }
+    }
+    
+    
+    @Override
+    public void delete(User user) {
+        LOGGER.debug("Deleting {}", user);
+        
+        try (SqlSession session = getSession()) {
+            try {
+                getUserMapper(session).delete(user);
+            } catch (RuntimeException e) {
+                LOGGER.info("Cannot delete {}", user, e);
+                session.rollback();
+                throw e;
+            }
+            
+            session.commit();
+        }
+    }
+    
+    
+    @Override
+    public void deleteAll() {
+        LOGGER.debug("Deleting all users");
+        
+        try (SqlSession session = getSession()) {
+            try {
+                getUserMapper(session).deleteAll();
+            } catch (RuntimeException e) {
+                LOGGER.info("Cannot delete all users", e);
+                session.rollback();
+                throw e;
+            }
+            
+            session.commit();
         }
     }
 }
