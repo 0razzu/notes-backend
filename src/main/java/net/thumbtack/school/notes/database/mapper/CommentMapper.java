521ba35f8804358ce882cb5d1c8f4743835fedd3
Дмитрий Патока <dmitrypatoka@ya.ru>
2020-11-21 01:37:01 +0600

    CommentMapper

--- src/main/java/net/thumbtack/school/notes/database/mapper/CommentMapper.java
+++ src/main/java/net/thumbtack/school/notes/database/mapper/CommentMapper.java
@@ -1,30 +1,29 @@
 package net.thumbtack.school.notes.database.mapper;
 
 
-import net.thumbtack.school.notes.model.Comment;
-import net.thumbtack.school.notes.model.NoteRevision;
-import net.thumbtack.school.notes.model.User;
+import net.thumbtack.school.notes.model.*;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.mapping.FetchType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface CommentMapper {
-    @Insert("INSERT INTO comment (created, body, author_id, note_revision_id)" +
+    @Insert("INSERT INTO note_comment (created, body, author_id, note_revision_id)" +
             "VALUES (#{created}, #{body}, #{author.id}, #{revision.id})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     Integer insert(Comment comment);
     
     
-    @Update("UPDATE comment SET created = #{created}, body = #{body}, revision_id = #{revision.id} " +
+    @Update("UPDATE note_comment SET created = #{created}, body = #{body}, revision_id = #{revision.id} " +
             "WHERE id = #{comment.id}")
     void update(Comment comment);
     
     
     @Select("SELECT (id, created, body, author_id, note_revision_id) " +
-            "FROM comment WHERE id = #{id}")
-    @Results({
+            "FROM note_comment WHERE id = #{id}")
+    @Results(id = "commentFields", value = {
             @Result(property = "id", column = "id"),
             @Result(property = "author", column = "author_id", javaType = User.class,
                     one = @One(
@@ -42,45 +41,34 @@ public interface CommentMapper {
     Comment get(int id);
     
     
-    @Select("SELECT (id, created, body, author_id, note_revision_id) " +
-            "FROM comment WHERE created = #{created}")
-    @Results({
-            @Result(property = "id", column = "id"),
-            @Result(property = "author", column = "author_id", javaType = User.class,
-                    one = @One(
-                            select = "net.thumbtack.school.notes.database.mappers.UserMapper.get",
-                            fetchType = FetchType.LAZY
-                    )
-            ),
-            @Result(property = "revision", column = "note_revision_id", javaType = NoteRevision.class,
-                    one = @One(
-                            select = "net.thumbtack.school.notes.database.mappers.RevisionMapper.get",
-                            fetchType = FetchType.LAZY
-                    )
-            )
-    })
-    Comment getByCreated(LocalDateTime created);
+    @Select("SELECT id, created, body, author_id, note_revision_id " +
+            "FROM note_comment WHERE created = #{created}")
+    @ResultMap("commentFields")
+    List<Comment> getByCreated(LocalDateTime created);
     
     
-    @Select("SELECT (id, created, body, author_id, note_revision_id) " +
-            "FROM comment WHERE author_id = #{author.id}")
-    @Results({
-            @Result(property = "id", column = "id"),
-            @Result(property = "author", column = "author_id", javaType = User.class,
-                    one = @One(
-                            select = "net.thumbtack.school.notes.database.mappers.UserMapper.get",
-                            fetchType = FetchType.LAZY
-                    )
-            ),
-            @Result(property = "revision", column = "note_revision_id", javaType = NoteRevision.class,
-                    one = @One(
-                            select = "net.thumbtack.school.notes.database.mappers.RevisionMapper.get",
-                            fetchType = FetchType.LAZY
-                    )
-            )
-    })
-    Comment getByAuthor(User author);
+    @Select("SELECT id, created, body, author_id, note_revision_id " +
+            "FROM note_comment WHERE author_id = #{author.id}")
+    @ResultMap("commentFields")
+    List<Comment> getByAuthor(User author);
     
     
-    // TODO: getByRevision / getByNote...
+    @Select("SELECT id, created, body, author_id, note_revision_id " +
+            "FROM note_comment WHERE section_id = #{section.id}")
+    @ResultMap("commentFields")
+    List<Comment> getBySection(Section section);
+    
+    
+    @Select("SELECT id, created, body, author_id, note_revision_id " +
+            "FROM note_comment WHERE note_revision_id = #{revision.id}")
+    @ResultMap("commentFields")
+    List<Comment> getByNoteRevision(NoteRevision revision);
+    
+    
+    @Select("SELECT note_comment.id, note_comment.created, note_comment.body, author_id, note_revision_id " +
+            "FROM note_comment " +
+            "JOIN note_revision ON note_comment.note_revision_id = note_revision.id " +
+            "WHERE note_id = #{note.id}")
+    @ResultMap("commentFields")
+    List<Comment> getByNote(Note note);
 }
