af97fef51fae6b1d2a9acab730b86fadc5e16d2b
Дмитрий Патока <dmitrypatoka@ya.ru>
2021-01-09 18:41:29 +0600

    TestRatingDao

---
+++ src/test/java/net/thumbtack/school/notes/database/daoimpl/TestRatingDao.java
@@ -0,0 +1,31 @@
+package net.thumbtack.school.notes.database.daoimpl;
+
+
+import net.thumbtack.school.notes.error.ServerException;
+import net.thumbtack.school.notes.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestRatingDao extends TestDaoBase {
+    @Test
+    void testInsert() throws ServerException {
+        User ann = new User("ann", "annann123", "Ann", null, "White", UserType.USER);
+        Section section = new Section("s1", ann);
+        Note note = new Note(ann, LocalDateTime.of(2021, 1, 1, 2, 9), section);
+        NoteRevision revision = new NoteRevision("n1", "some text", LocalDateTime.of(2021, 1, 1, 3, 0), note);
+        Rating rating = new Rating(5, admin);
+        
+        userDao.insert(ann);
+        sectionDao.insert(section);
+        noteDao.insert(note, revision);
+        
+        ratingDao.insert(rating, note);
+        
+        assertEquals(List.of(rating), noteDao.get(note.getId()).getRatings());
+    }
+}
